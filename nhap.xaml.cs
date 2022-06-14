079396b3b93947e7302ca098710dcdf88dd4dcb0
Nguyễn Đình Nguyên <nguyendinhnguyen39@gmail.com>
2022-06-14 10:25:25 +0700

    hi

---
+++ nhap.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for nhap.xaml
+    /// </summary>
+    public partial class nhap : Window
+    {
+        public nhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
